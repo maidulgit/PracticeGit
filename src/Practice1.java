c4d77b18332271ab16cb18068da3cd38c5b4346e
maidulgit <maidul360@gmail.com>
2018-09-09 01:19:25 -0400

    first try

---
+++ src/Practice1.java
@@ -0,0 +1,7 @@
+import java.sql.SQLOutput;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.print("Lets check");
+    }
+}
---
+++ src/Practice2.java
@@ -0,0 +1,6 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        System.out.println("Lets Check");
+    }
+
+}
